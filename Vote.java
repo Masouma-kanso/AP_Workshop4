fa6ad88400bb2e046f97b3367ccfb4913e175b91
Masouma-kanso <masoumakanso@gmail.com>
2024-12-13 07:01:33 -0800

    added hashcode method to Vote

--- Vote.java
+++ Vote.java
@@ -1,7 +1,7 @@
 import java.util.Objects;
 
 public class Vote {
-    private final Person voter;
+    private final Person  voter;
     private final String date;
 
 
@@ -25,4 +25,8 @@ public class Vote {
         return Objects.equals(voter, vote.voter) && Objects.equals(date, vote.date);
     }
 
-}
+    @Override
+    public int hashCode() {
+        return Objects.hash(voter, date);
+    }
+    }
