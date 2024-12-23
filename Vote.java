d8e03bb9389b5353762d3a8d93311f3ab4f60fcf
Masouma-kanso <masoumakanso@gmail.com>
2024-12-23 10:31:39 -0800

    created Vote class

--- Vote.java
+++ Vote.java
@@ -1,33 +1,5 @@
-import java.util.Objects;
 
 public class Vote {
-    private final Person voter;
- private final String date;
 
 
-    public Vote(Person voter, String date) {
-        this.voter = voter;
-        this.date = date;
-    }
-
-    public Person getVoter() {
-        return voter;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof VotingSystem)) return  false;
-        VotingSystem that = (VotingSystem) 0;
-        return Objects.equals(getVotingList(), that.getVotingList());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getVotingList());
-    }
 }
