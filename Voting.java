10192a2266d78429f0d8e4af4cf7789230562d5d
Masouma-kanso <masoumakanso@gmail.com>
2024-12-23 12:25:53 -0800

    added feilds Vote class

--- Voting.java
+++ Voting.java
@@ -1,2 +1,9 @@
 public class Voting {
+    private int type;
+    private String question;
+    private HashMap<String, HashSet<Vote>> choices;
+    private boolean isAnonymous;
+    private ArrayList<Person> voters;
+
+
 }
