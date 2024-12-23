9b6b0bd485f055dbfeaa37ceed75faf1cc905867
Masouma-kanso <masoumakanso@gmail.com>
2024-12-23 10:33:43 -0800

    add getters to Vote

--- Vote.java
+++ Vote.java
@@ -2,10 +2,19 @@
 public class Vote {
     private final Person voter;
     private final String date;
-    
+
     public Vote(Person voter, String date) {
         this.voter = voter;
         this.date = date;
     }
+    public Person getVoter() {
+        return voter;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+
 
 }
