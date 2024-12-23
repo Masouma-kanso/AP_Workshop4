60b3fa50dbbf0960fd27000a3620d7a13a3cb2c3
Masouma-kanso <masoumakanso@gmail.com>
2024-12-23 10:33:11 -0800

    add a constractor to Vote

--- Vote.java
+++ Vote.java
@@ -2,5 +2,10 @@
 public class Vote {
     private final Person voter;
     private final String date;
+    
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
 
 }
